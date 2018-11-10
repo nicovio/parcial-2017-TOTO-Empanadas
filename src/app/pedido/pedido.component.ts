4f2b7540e33099bdb09a5b3edb51e7dd802d1ac1
nicovio <nicolasviotti90@gmail.com>
2018-11-10 18:52:58 -0300

    Incorporando punto 3

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import Empanada from '../domain/empanada';
 import { EmpanadasService } from '../servicios/empanadas.service';
 import { Router } from "@angular/router"
-import Pedido from '../domain/pedido';
-import { text } from '@angular/core/src/render3';
 
 @Component({
   selector: 'pedido',
@@ -14,6 +12,7 @@ export class PedidoComponent implements OnInit {
 
   empanadas: Array<Empanada> = []
   textoBusqueda: string = ''
+  errorMenosDe4Empanadas = false
 
   constructor(private empanadasService: EmpanadasService, private router: Router) {
     this.empanadas = empanadasService.getEmpanadas()
@@ -35,9 +34,21 @@ export class PedidoComponent implements OnInit {
   }
 
   finalizarPedido() {
+    if(this.cantidadDeEmpanadas() >= 4 ){
     const pedido = this.empanadasService.crearPedido(this.empanadas)
     this.empanadasService.agregarPedido(pedido)
     this.navegarAFinalizacionPedido(pedido.id)
+    }
+    else{
+      this.errorMenosDe4Empanadas = true
+    }
+  }
+
+  cantidadDeEmpanadas(){
+    const cantidadTotal = this.empanadas.map(empanada => empanada.cantidad).reduce((sum, current) => sum + current);
+    return cantidadTotal
+
+
   }
 
   navegarAFinalizacionPedido(idPedido: number) {
